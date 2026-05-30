287a031d08614bb084867e69010cbe78f729872b
Maurin Saurio <mauromarchesan88@gmail.com>
2026-05-30 16:46:22 -0300

    checking build

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div class="img-container">
-          <img src={require('./images/img1.jpg')} alt="Example Image"/>
-          <a href="https://wa.me/+556596023456" class="whatsapp-btn" target="_blank" aria-label="Chat on WhatsApp"/>
+          <img src={require('./images/img1.jpg')} alt="azul"/>
+          <a href="https://wa.me/+556596023456" class="whatsapp-btn" aria-label="Chat on WhatsApp"/>
         </div>
-        <img src={require('./images/img2.jpg')} className="img-2" alt="img2"/>
+        <img src={require('./images/img2.jpg')} className="img-2" alt="cristo"/>
       </header>
     </div>
   );
